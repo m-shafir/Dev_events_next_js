1869a0b9b9d70446d0024394bbbc82c299006c5d
muhammed shafir <muhammedshafirweb@gmail.com>
2025-10-26 14:13:41 +0530

    event card data showing design completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-
-const events = [
-  { image: "/images/event1.png", title: "Event 1" },
-  { image: "/images/event2.png", title: "Event 2" },
-];
+import { events } from "@/lib/constants";
 
 function Home() {
   return (
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,14 +1,19 @@
+import { ImageDown } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
   title: string;
   image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
 }
 
-function EventCard({ title, image }: Props) {
+function EventCard({ title, image, slug, location, date, time }: Props) {
   return (
-    <Link href="/events" id="event-card">
+    <Link href={`/${slug}`} id="event-card">
       <Image
         src={image}
         alt={title}
@@ -16,7 +21,21 @@ function EventCard({ title, image }: Props) {
         height={300}
         className="poster"
       />
+      <div className="flex flex-wor gap-2">
+        <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
       <p className="title">{title}</p>
+      <div className="datetime">
+        <div>
+          <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
+        </div>
+        <div>
+          <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
+        </div>
+      </div>
     </Link>
   );
 }
